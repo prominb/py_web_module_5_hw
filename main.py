fcfca14e25001ebbd7c64c77a502d4be4923c2ee
Roman M <prominb@gmail.com>
2024-04-07 04:58:03 +0300

    edit: add check days

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ class HttpError(Exception):
 async def request(url: str):
     async with httpx.AsyncClient() as client:
         r = await client.get(url)
-        # test = await client.get('https://www.google.com')
-        # print(test.text)
         if r.status_code == 200:
             result = r.json()
             return result
@@ -23,8 +21,11 @@ async def request(url: str):
 
 
 async def main(index_day):
-    # d = datetime.now() - timedelta(day=2) -> d.strftime("%d.%m.%Y")
-    d = datetime.now() - timedelta(days=int(index_day))
+    # if int(index_day) > 10:
+        # raise ValueError("Days less 10.")
+        # print(f"Days {index_day} more then 10")
+        # sys.exit(f"Days {index_day} more then 10")
+    d = datetime.now() - timedelta(days=index_day)
     shift = d.strftime("%d.%m.%Y")
     try:
         response = await request(f'https://api.privatbank.ua/p24api/exchange_rates?date={shift}')
@@ -37,6 +38,10 @@ async def main(index_day):
 if __name__ == '__main__':
     if platform.system() == 'Windows':
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    # print(sys.argv)
-    r = asyncio.run(main(sys.argv[1]))
+    if len(sys.argv) < 2:
+        sys.exit("You need enter Days.")
+    get_argv = int(sys.argv[1])
+    if get_argv > 10:
+        sys.exit(f"Days {get_argv} more then 10.")
+    r = asyncio.run(main(get_argv))
     print(r)
